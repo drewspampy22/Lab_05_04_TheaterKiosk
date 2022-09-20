357b8da376d1e578e64ce020493bcbf291ffad0b
drewspampy22 <drewspampy22@gmail.com>
2022-09-19 20:31:45 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+       int Age=18;
+
+        if (Age >= 21)
+        {
+            System.out.println("You got a wrist band");
+        }
+        else {
+
+        }
+    }
+}
